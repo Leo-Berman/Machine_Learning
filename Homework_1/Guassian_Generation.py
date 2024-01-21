0731b0eb398f32760f76b352463a9a002ffb02e9
leo-berman <leo.berman@temple.edu>
2024-01-21 12:20:43 -0500

    more debugging

--- Homework_1/Guassian_Generation.py
+++ Homework_1/Guassian_Generation.py
@@ -1,7 +1,6 @@
 import numpy
 import pandas
-def Gen_2d_Gaussian(mean,cov,n_elem):
-        return numpy.random.multivariate_normal(mean,cov,n_elem)
+import matplotlib.pyplot as plt
 
 def main():
     # number of elements
@@ -17,24 +16,31 @@ def main():
     cov_1 = [[0.1, 0],[0,0.1]]
     cov_2 = [[1, 0],[0,1]]
 
-    # generate the gaussians
-    cov_1_class_1 = numpy.array(Gen_2d_Gaussian(mean_1,cov_1,number_elements).tolist()).reshape((2,number_elements))
-    cov_1_class_2 = numpy.array(Gen_2d_Gaussian(mean_2,cov_1,number_elements).tolist()).reshape((2,number_elements))
-    cov_1_class_3 = numpy.array(Gen_2d_Gaussian(mean_3,cov_1,number_elements).tolist()).reshape((2,number_elements))
-    cov_1_class_4 = numpy.array(Gen_2d_Gaussian(mean_4,cov_1,number_elements).tolist()).reshape((2,number_elements))
-    
-    cov_2_class_1 = numpy.array(Gen_2d_Gaussian(mean_1,cov_2,number_elements).tolist()).reshape((2,number_elements))
-    cov_2_class_2 = numpy.array(Gen_2d_Gaussian(mean_2,cov_2,number_elements).tolist()).reshape((2,number_elements))
-    cov_2_class_3 = numpy.array(Gen_2d_Gaussian(mean_3,cov_2,number_elements).tolist()).reshape((2,number_elements))
-    cov_2_class_4 = numpy.array(Gen_2d_Gaussian(mean_4,cov_2,number_elements).tolist()).reshape((2,number_elements))
-    
     classes = ["1"]*number_elements+["2"]*number_elements+["3"]*number_elements+["4"]*number_elements
-    cov_1_col1 = numpy.concatenate((cov_1_class_1[0], cov_1_class_2[0], cov_1_class_3[0], cov_1_class_4[0]))
-    cov_1_col2 = numpy.concatenate((cov_1_class_1[1], cov_1_class_2[1], cov_1_class_3[1], cov_1_class_4[1]))
-    cov_2_col1 = numpy.concatenate((cov_2_class_1[0], cov_2_class_2[0], cov_2_class_3[0], cov_2_class_4[0]))
-    cov_2_col2 = numpy.concatenate((cov_2_class_1[1], cov_2_class_2[1], cov_2_class_3[1], cov_2_class_4[1]))
+    cov_1_class_1 = numpy.random.multivariate_normal(mean_1,cov_1,number_elements)
+    cov_1_class_2 = numpy.random.multivariate_normal(mean_2,cov_1,number_elements)
+    cov_1_class_3 = numpy.random.multivariate_normal(mean_3,cov_1,number_elements)
+    cov_1_class_4 = numpy.random.multivariate_normal(mean_4,cov_1,number_elements)
+    cov_2_class_1 = numpy.random.multivariate_normal(mean_1,cov_2,number_elements)
+    cov_2_class_2 = numpy.random.multivariate_normal(mean_2,cov_2,number_elements)
+    cov_2_class_3 = numpy.random.multivariate_normal(mean_3,cov_2,number_elements)
+    cov_2_class_4 = numpy.random.multivariate_normal(mean_4,cov_2,number_elements)
 
-
+    cov_1_col_1 = cov_1_class_1[:,0].tolist() + cov_1_class_2[:,0].tolist() + cov_1_class_3[:,0].tolist() + cov_1_class_4[:,0].tolist()
+    cov_1_col_2 = cov_1_class_1[:,1].tolist() + cov_1_class_2[:,1].tolist() + cov_1_class_3[:,1].tolist() + cov_1_class_4[:,1].tolist()
+    cov_2_col_1 = cov_2_class_1[:,0].tolist() + cov_2_class_2[:,0].tolist() + cov_2_class_3[:,0].tolist() + cov_2_class_4[:,0].tolist()
+    cov_2_col_2 = cov_2_class_1[:,1].tolist() + cov_2_class_2[:,1].tolist() + cov_2_class_3[:,1].tolist() + cov_2_class_4[:,1].tolist()
+    
+    
+    print(cov_1_col_1)
+    plt.plot(cov_1_class_1[:, 0], cov_1_class_1[:, 1], '.', alpha=0.5)
+    plt.plot(cov_1_class_2[:, 0], cov_1_class_2[:, 1], '.', alpha=0.5)
+    plt.plot(cov_1_class_3[:, 0], cov_1_class_3[:, 1], '.', alpha=0.5)
+    plt.plot(cov_1_class_4[:, 0], cov_1_class_4[:, 1], '.', alpha=0.5)
+    plt.axis('equal')
+    plt.grid()
+    plt.show()
+    
     d_1 = {
         'col0':classes, 
         'col1':cov_1_col1,
