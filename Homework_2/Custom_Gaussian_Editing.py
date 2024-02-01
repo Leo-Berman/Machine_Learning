3f1742d0a3c1ab9e992e278bcc9d42ffac3d2cd6
Leo Grant Berman <tuo54571@ece-000.eng.temple.edu>
2024-02-01 11:54:28 -0500

    Using homework 1 to generate debug file

--- Homework_2/Custom_Gaussian_Editing.py
+++ Homework_2/Custom_Gaussian_Editing.py
@@ -1,32 +1,34 @@
 
 import numpy as np
 import pandas as pd
+import sys
 class Feature:
 
     # initialize values
     def __init__(self,inname):
-        self.class_name = inname
-        self.number_elements = 0
-        self.x_mean = None
-        self.y_mean = None
-        self.x_sd = None
-        self.y_sd = None
-        self.data = []
-        self.cov = 0
-        self.cov_mat = []
-        self.x_var = 0
-        self.y_var = 0
+        self.class_name =       inname
+        self.number_elements =  0
+        self.x_mean =           None
+        self.y_mean =           None
+        self.x_sd =             None
+        self.y_sd =             None
+        self.data =             np.array([])
+        self.cov =              None
+        self.cov_mat =          None
+        self.x_var =            None
+        self.y_var =            None
+        self.mean_vec =         None
     
     # printing self
     def print_self(self):
         print(self.class_name,":")
         print("Number of elements = ", self.number_elements)
         print("X mean = ",self.x_mean)
-        print("X Standard Deviation = ",self.x_sd)
+        #print("X Standard Deviation = ",self.x_sd)
         print("Y mean = ",self.y_mean)
-        print("Y Standard Deviation = ",self.y_sd)
+        #print("Y Standard Deviation = ",self.y_sd)
         print("Covariance = ",self.cov)
-        print("Covraiance Matrix = \n",self.cov_mat)
+        print("Covariance Matrix = \n",self.cov_mat)
 
 
     # calculating the standard deviation
@@ -41,7 +43,12 @@ class Feature:
 
     # Add in feature data
     def add_data(self,indata):
-        self.data.append(indata)
+        #print(indata)
+        if len(self.data) == 0:
+            self.data = indata
+        else:
+            self.data=np.vstack([self.data, indata])
+        #print(self.data)
         self.number_elements+=1
 
     # Calculating the mean of each feature of the class
@@ -53,22 +60,38 @@ class Feature:
             y_sum+=x[1]
         self.x_mean = x_sum/self.number_elements
         self.y_mean = y_sum/self.number_elements
-        
+
+    def calculate_mean_vector(self):
+        self.calculate_mean()
+        self.mean_vec = np.array([self.x_mean,self.y_mean])
+
     # Calculating covariance
     def calculate_covariance(self):
         work = 0
         for x in self.data:
-            work+=(x[0]-self.x_mean) * (x[1]-self.y_mean)
-        self.cov = work/(self.number_elements-1)
+            work+=(x[0] - self.x_mean) * (x[1] - self.y_mean)
+        self.cov = work / (self.number_elements-1)
 
     # Calculating covariance matrix
     def calculate_covariance_matrix(self):
+        self.calculate_covariance()
+        self.calculate_variance()
         self.cov_mat = np.array([[self.x_var, self.cov],[self.cov, self.y_var]])
 
+        #print("Hand calc = \n",self.cov_mat)
+        #print(self.data[:,0])
+        #print("Numpy = \n",np.cov(self.data[:,0],self.data[:,1]))
     # Calculating population variance
     def calculate_variance(self):
-        self.x_var = self.x_sd**2
-        self.y_var = self.y_sd**2
+        x_sum = 0
+        y_sum = 0
+        for x in self.data:
+            x_sum+=(x[0]-self.x_mean)**2
+            y_sum+=(x[1]-self.y_mean)**2
+
+        self.x_var = x_sum/(self.number_elements-1)
+        self.y_var = y_sum/(self.number_elements-1)
+
 class Custom_Gaussian:
     
     # constructor
@@ -96,17 +119,14 @@ class Custom_Gaussian:
                 self.classes[training_data[i][0]]=Feature(training_data[i][0])
             
             # Add data to feature
-            self.classes[training_data[i][0]].add_data([float(training_data[i][1]),float(training_data[i][2])])
+            self.classes[training_data[i][0]].add_data(np.array([float(training_data[i][1]),float(training_data[i][2])]))
 
             # Keep track of total number of elements
             self.number_elements+=1
 
         # Call functions for calculating means and standard deviations
         for x in self.classes:
-            self.classes[x].calculate_mean()
-            self.classes[x].calculate_sd()
-            self.classes[x].calculate_covariance()
-            self.classes[x].calculate_variance()
+            self.classes[x].calculate_mean_vector()
             self.classes[x].calculate_covariance_matrix()
 
     # printing data from the class
@@ -119,14 +139,12 @@ class Custom_Gaussian:
     def return_probability(self,inclass,inx,iny):
         guess_elements = -1
         guess = None
-
+        guess_prob = -1*float("inf")
         # Iterate through all classes and calculate each features probability density
         for x in self.classes:
-            guess_prob = -1
+            
             # Mean vector            
-            x_mean = self.classes[x].x_mean
-            y_mean = self.classes[x].y_mean
-            mean_vector = np.array([x_mean,y_mean])
+            mean_vector = self.classes[x].mean_vec
 
             # Feature vector
             feature_vector = np.array([inx,iny])
@@ -134,27 +152,28 @@ class Custom_Gaussian:
             # Covariance matrix
             covariance_matrix = self.classes[x].cov_mat
             
+
             # Print for debugging
-            #print("mean vector = \n",mean_vector,"\n")
+            print(x,"mean vector = \n",mean_vector,"\n")
             #print("feature vector = \n",feature_vector,"\n")
-            #print("covariance matrix = \n",covariance_matrix,"\n")
+            print(x,"covariance matrix = \n",covariance_matrix,"\n")
 
             #print("DEBUG = \n",np.dot(feature_vector-mean_vector,np.linalg.inv(covariance_matrix)))
             # Elements for calculating probability density
-            ele1_1 = -.5*np.dot(np.transpose(feature_vector-mean_vector),np.linalg.inv(covariance_matrix))
+            ele1_1 = np.matmul(np.transpose(feature_vector-mean_vector),np.linalg.inv(covariance_matrix))
             ele1_2 = feature_vector-mean_vector
-            ele1 = np.dot(ele1_1,ele1_2)
+            ele1 = -.5 * np.matmul(ele1_1,ele1_2)
             #print("Element 1 = \n",ele1,"\n")
-            ele2 = -.5*np.log(2*np.pi)
+            ele2 = -.5 * np.log(2*np.pi)
             #print("Element 2 = \n",ele2,"\n")
-            ele3 = -.5*(np.log(np.linalg.det(covariance_matrix)))
+            ele3 = -.5 * (np.log(np.linalg.det(covariance_matrix)))
             #print("Element 3 = \n",ele3,"\n")
             ele4 = np.log(1/self.classes[x].number_elements)
             #ele4 = np.log(.5)
             #print("Element 4 = \n",ele4,"\n")
 
             # Calculate probability density
-            total_prob = ele1 + ele2 + ele3 #+ ele4
+            total_prob = ele1 + ele3 + ele2 + ele3 + ele4
             #print("score = \n",total_prob,"\n")
 
             # If probability is greater than last guess set as new guess
@@ -177,16 +196,13 @@ class Custom_Gaussian:
         for x in newdata:
             if (self.return_probability(x[0],float(x[1]),float(x[2]))) == True:
                 total_correct += 1
-            else:
-                total_wrong += 1
         accuracy_rate = total_correct/self.number_elements
         return accuracy_rate
 
-def main():
-    
+def hw_data():
     # read data in and turn it into a 3 column numpy array
-    train = pd.read_csv("train.csv",comment = "#").to_numpy()
-    eval = pd.read_csv("eval.csv", comment = "#").to_numpy()
+    train = pd.read_csv("debug2.csv",comment = "#").to_numpy()
+    eval = pd.read_csv("debug2.csv", comment = "#").to_numpy()
     train = np.array(list(zip(train[:,0],train[:,1],train[:,2])))
     eval = np.array(list(zip(eval[:,0],eval[:,1],eval[:,2])))
 
@@ -194,11 +210,35 @@ def main():
     my_gauss = Custom_Gaussian()
     my_gauss.train(train)
     #my_gauss.train(eval)
-    my_gauss.print_classes()
-    #my_gauss.eval([["dogs",0.002100,-0.434914]])
+    #my_gauss.print_classes()
+    #print("Test Case = ",my_gauss.eval([["dogs",-50,-50]]))
     
-    print("Evaluation data accuracy = ", 1-my_gauss.eval(eval))
-    print("Training data accuracy = ", 1-my_gauss.eval(train))
+    print("Evaluation accuracy rate = ", 1-my_gauss.eval(eval))
+    print("Training accuracy rate = ", 1-my_gauss.eval(train))
+
+def debug_data():
+    # read data in and turn it into a 3 column numpy array
+    train = pd.read_csv("/home/tuo54571/Machine_Learning/Homework_1/cov_1.csv",comment = "#").to_numpy()
+    eval = pd.read_csv("/home/tuo54571/Machine_Learning/Homework_1/cov_2.csv", comment = "#").to_numpy()
+    train = np.array(list(zip(train[:,0],train[:,1],train[:,2])))
+    eval = np.array(list(zip(eval[:,0],eval[:,1],eval[:,2])))
+
+    # initialize model
+    my_gauss = Custom_Gaussian()
+    my_gauss.train(train)
+    #my_gauss.train(eval)
+    #my_gauss.print_classes()
+    #print("Test Case = ",my_gauss.eval([["dogs",-50,-50]]))
+    
+    print("Evaluation accuracy rate = ", 1-my_gauss.eval(eval))
+    print("Training accuracy rate = ", 1-my_gauss.eval(train))
+def main():
+    if len(sys.argv) == 1:
+        hw_data()
+    elif sys.argv[1] == "-debug":
+        debug_data()
+    else:
+        print("Use Sklearn_QDA.py as:\n'python3 Sklearn_QDA.py -debug' or 'python3 Sklearn QDA.py'")
     
 
 
--- Homework_2/Sklearn_QDA.py
+++ Homework_2/Sklearn_QDA.py
@@ -1,9 +1,8 @@
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis as QDA
 import numpy as np
 import pandas as pd
-
-def main():
-
+import sys
+def hw_data():
     # read in the data from the csv files
     train = pd.read_csv("train.csv",comment = "#").to_numpy()
     eval = pd.read_csv("eval.csv", comment = "#").to_numpy()
@@ -13,11 +12,35 @@ def main():
 
     # Set and train the algorithm
     algo = QDA()
-    print(algo.fit(train_coords,train[:,0]))
+    algo.fit(train_coords,train[:,0])
 
     # Evaluate the algorithm
     print("Evaluation accuracy rate: ",1-algo.score(eval_coords,eval[:,0]))
     print("Training accuracy rate: ",1-algo.score(train_coords,train[:,0]))
+
+def debug_data():
+    # read in the data from the csv files
+    train = pd.read_csv("/home/tuo54571/Machine_Learning/Homework_1/cov_1.csv",comment = "#").to_numpy()
+    eval = pd.read_csv("/home/tuo54571/Machine_Learning/Homework_1/cov_2.csv", comment = "#").to_numpy()
+    
+    train_coords = np.array(list(zip(train[:,1],train[:,2])))
+    eval_coords = np.array(list(zip(eval[:,1],eval[:,2])))
+
+    # Set and train the algorithm
+    algo = QDA()
+    algo.fit(train_coords,train[:,0])
+
+    # Evaluate the algorithm
+    print("Evaluation accuracy rate: ",1-algo.score(eval_coords,eval[:,0]))
+    print("Training accuracy rate: ",1-algo.score(train_coords,train[:,0]))
+def main():
+    
+    if len(sys.argv) == 1:
+        hw_data()
+    elif sys.argv[1] == "-debug":
+        debug_data()
+    else:
+        print("Use Sklearn_QDA.py as:\n'python3 Sklearn_QDA.py -debug' or 'python3 Sklearn QDA.py'")
 
 
 main()
