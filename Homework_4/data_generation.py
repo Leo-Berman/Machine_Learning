e3eb988ce3c3a7b1d6fa52b395d9eac022ea8ba1
Leo Grant Berman <tuo54571@ece-000.eng.temple.edu>
2024-02-15 10:15:04 -0500

    calculating pdf

--- Homework_4/data_generation.py
+++ Homework_4/data_generation.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import sklearn as skl
 import sys
 import os
+from scipy.stats import norm
 
 def plot_histogram(data,name):
     # Plotting a basic histogram
@@ -15,24 +16,36 @@ def plot_histogram(data,name):
     plt.title('Basic Histogram')
     
     # Display the plot
-    plt.savefig(name+".png")
-    
+    #plt.savefig(name+".png")
+    #print("saved")
 def generate_data(means,variances,nelems):
     ret = []
     [ret.append(np.random.normal(loc = x,scale = y,size = nelems)) for x,y in zip(means,variances)]
     return ret
 
 def main():
-    # number of elements
+    # number of elements, means and variances
     nelems = 10000
-
-    # declare means and variances
     means = [-2.0,0.0,2.0]
     variances = [1.0,0.5,1.0]
     
     # generate data vectors
     vectors = generate_data(means,variances,nelems)
 
+    # concatentae all vectors into 30,000 element list
+    total_points = []
+    [total_points.extend(x) for x in vectors]
+    print(len(total_points))
+
+    # calculate the pdf
+    pdf = norm.pdf(total_points, np.mean(total_points), np.mean(variances))
+    plot_histogram(total_points,"q1")
+    plt.plot(total_points, pdf, label='Distribuzione Normale')
+    plt.title('PDF of the Normal Distribution')
+    plt.xlabel('Value')
+    plt.ylabel('Probability')
+    plt.legend()
+    plt.show()
     
 
 main()
