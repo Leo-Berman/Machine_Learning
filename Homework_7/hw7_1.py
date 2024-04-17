22389aac98f7bc5458e51126bc45e6c00b1a4549
leo-berman <leo.berman@temple.edu>
2024-04-17 12:38:56 -0400

    updates to homeworks

--- Homework_7/hw7_1.py
+++ Homework_7/hw7_1.py
@@ -21,7 +21,7 @@ def calculate_1d_entropy(class_list):
         probs[x]=class_list.count(x)/len(class_list)
 
     entropy = 0
-    for x in class_list:
+    for x in probs:
         entropy-=probs[x]*np.log(probs[x])
     return entropy
 
@@ -34,7 +34,7 @@ def calculate_2d_entropy(class_list1,class_list2):
         probs[x]=l.count(x)/len(l)
 
     entropy = 0
-    for x in l:
+    for x in probs:
         entropy-=probs[x]*np.log(probs[x])
     return entropy
 
@@ -60,6 +60,10 @@ def main():
     quantized_dev_x1 = quantize_list(dev_x1)
     quantized_dev_x2 = quantize_list(dev_x2)
 
+    rng = np.random.default_rng()
+    quantized_unif_x1 = rng.integers(0,129,1000).tolist()
+    quantized_unif_x2 = rng.integers(0,129,1000).tolist()
+    
 
     
     
@@ -69,32 +73,53 @@ def main():
     H_x2_train = calculate_1d_entropy(quantized_train_x2)
     H_x1_dev = calculate_1d_entropy(quantized_dev_x1)
     H_x2_dev = calculate_1d_entropy(quantized_dev_x2)
+    H_x1_unif = calculate_1d_entropy(quantized_unif_x1)
+    H_x2_unif = calculate_1d_entropy(quantized_unif_x2)
+
+
     
     H_x1x2_train = calculate_2d_entropy(quantized_train_x1,quantized_train_x2)
     H_x1x2_dev  = calculate_2d_entropy(quantized_dev_x1,quantized_dev_x2)
+    H_x1x2_unif  = calculate_2d_entropy(quantized_unif_x1,quantized_unif_x2)
 
     train_mutualinfo = H_x1_train + H_x2_train - H_x1x2_train
     dev_mutualinfo   = H_x1_dev + H_x2_dev - H_x1x2_dev
-
+    unif_mutualinfo   = H_x1_unif + H_x2_unif - H_x1x2_unif
+    
     H_x1x2_train_conditional = H_x1_train - train_mutualinfo
     H_x2x1_train_conditional = H_x2_train - train_mutualinfo
-
+    
+    
     H_x1x2_dev_conditional = H_x1_dev - dev_mutualinfo
     H_x2x1_dev_conditional = H_x2_dev - dev_mutualinfo
 
+    H_x1x2_unif_conditional = H_x1_unif - unif_mutualinfo
+    H_x2x1_unif_conditional = H_x2_unif - unif_mutualinfo
+    
+    
     print("Train H(x1)".ljust(20), "= ",H_x1_train)
     print("Train H(x2)".ljust(20), "= ",H_x2_train)
     print("Train H(x1,x2)".ljust(20),"= ",H_x1x2_train)
     print("Train H(x1|x2)".ljust(20),"= ",H_x1x2_train_conditional)
     print("Train H(x2|x1)".ljust(20),"= ",H_x2x1_train_conditional) 
-
+    print("Train I(x1,x2)".ljust(20),"= ",train_mutualinfo)
     print("\n")
     
     print("Dev H(x1)".ljust(20), "= ",H_x1_dev)
     print("Dev H(x2)".ljust(20), "= ",H_x2_dev)
     print("Dev H(x1,x2)".ljust(20),"= ",H_x1x2_dev)
-    print("Dev H(x1|x2)".ljust(20),"= ",H_x1x2_train_conditional)
-    print("Dev H(x2|x1)".ljust(20),"= ",H_x2x1_train_conditional) 
-    
+    print("Dev H(x1|x2)".ljust(20),"= ",H_x1x2_dev_conditional)
+    print("Dev H(x2|x1)".ljust(20),"= ",H_x2x1_dev_conditional) 
+    print("Dev I(x1,x2)".ljust(20),"= ",dev_mutualinfo)
+    print("\n")
+
+        
+    print("Unif H(x1)".ljust(20), "= ",H_x1_unif)
+    print("Unif H(x2)".ljust(20), "= ",H_x2_unif)
+    print("Unif H(x1,x2)".ljust(20),"= ",H_x1x2_unif)
+    print("Unif H(x1|x2)".ljust(20),"= ",H_x1x2_unif_conditional)
+    print("Unif H(x2|x1)".ljust(20),"= ",H_x2x1_unif_conditional) 
+    print("Unif I(x1,x2)".ljust(20),"= ",unif_mutualinfo)
+    print("\n")
 if __name__ == "__main__":
     main()
--- Homework_8/hw8.py
+++ Homework_8/hw8.py
@@ -1,12 +1,16 @@
+#!/usr/bin/env python
+
 from pathlib import Path
 import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis as QDA
 from sklearn.cluster import KMeans as KNM
 from sklearn.neighbors import KNeighborsClassifier as KNN
 from sklearn.ensemble import RandomForestClassifier as RNF
+from sklearn.decomposition import PCA as PCA
 def get_classes_features(data:pd.DataFrame):
 
     # get the classes into a 1d numpy array
@@ -18,6 +22,11 @@ def get_classes_features(data:pd.DataFrame):
     feature_vectors  =   np.stack([np.array(data['x1']),np.array(data['x2'])],axis=1)
     return classes,feature_vectors
 
+def score_model(train:pd.DataFrame,dev:pd.DataFrame,eval:pd.DataFrame,name:str,model_type:str):
+    model = PCA()
+    '''
+    fit and score model
+    '''
 def score_model(train:pd.DataFrame,dev:pd.DataFrame,eval:pd.DataFrame,name:str,model_type:str):
     
     
@@ -31,6 +40,8 @@ def score_model(train:pd.DataFrame,dev:pd.DataFrame,eval:pd.DataFrame,name:str,m
         model = LDA()
     elif model_type == "RNF":
         model = RNF()
+    elif model_type == "QDA":
+        model = QDA()
     else:
         print("Invalid model type")
         return
@@ -89,9 +100,10 @@ def score_knn(train:pd.DataFrame,dev:pd.DataFrame,eval:pd.DataFrame,name:str):
     plot_knn(x_axis,train_scores,"Training"+name)
     plot_knn(x_axis,dev_scores,"Development"+name)
     plot_knn(x_axis,eval_scores,"Evaluation"+name)
+
     
-    print(name,"KNN",":\n\tTraining (Number of Neighbors = ",train_scores.index(max(train_scores)),") : ", max(train_scores),"\n\tDevelopment (Number of Neighbors = ",dev_scores.index(max(dev_scores)),") : ", max(dev_scores),"\n\tEvaluation (Number of Neighbors = ",eval_scores.index(max(eval_scores)),") : ", max(eval_scores))
-    pass
+    ideal_dev = dev_scores.index(max(dev_scores))
+    print(name,"KNN",":\n\tTraining (Number of Neighbors = ",ideal_dev,") : ", train_scores[ideal_dev],"\n\tDevelopment (Number of Neighbors = ",ideal_dev,") : ", dev_scores[ideal_dev],"\n\tEvaluation (Number of Neighbors = ",ideal_dev,") : ", eval_scores[ideal_dev])
 
 def score_knm(train:pd.DataFrame,dev:pd.DataFrame,eval:pd.DataFrame,name:str):
     train_classes,train_features = get_classes_features(train)
@@ -139,8 +151,10 @@ def score_knm(train:pd.DataFrame,dev:pd.DataFrame,eval:pd.DataFrame,name:str):
     plot_knm(x_axis,dev_scores,"Development"+name)
     plot_knm(x_axis,eval_scores,"Evaluation"+name)
     
-    print(name,"KNM",":\n\tTraining (Number of Clusters = ",train_scores.index(max(train_scores)),") : ", max(train_scores),"\n\tDevelopment (Number of Clusters = ",dev_scores.index(max(dev_scores)),") : ", max(dev_scores),"\n\tEvaluation (Number of Clusters = ",eval_scores.index(max(eval_scores)),") : ", max(eval_scores))
-    pass
+    
+    ideal_dev = dev_scores.index(max(dev_scores))
+    print(name,"KNM",":\n\tTraining (Number of Clusters = ",ideal_dev,") : ", train_scores[ideal_dev],"\n\tDevelopment (Number of Clusters = ",ideal_dev,") : ", dev_scores[ideal_dev],"\n\tEvaluation (Number of Clusters = ",ideal_dev,") : ", eval_scores[ideal_dev])
+
 
 def get_parent_dir():
 
@@ -185,35 +199,51 @@ def main():
     #
     set_8_path = os.path.join(data_folder, "Set_08")
     set_8_train,set_8_dev,set_8_eval = get_csv(set_8_path)
-
+    set_8_traindev = np.vstack([set_8_train,set_8_dev])
+    print("Set 8 TrainDev = ",set_8_traindev)
     set_9_path = os.path.join(data_folder, "Set_09")
     set_9_train,set_9_dev,set_9_eval = get_csv(set_9_path)
 
     set_10_path = os.path.join(data_folder, "Set_10")
     set_10_train,set_10_dev,set_10_eval = get_csv(set_10_path)
 
+    # score the pca for each set
+    #
+    score_pca(set_8_train,set_8_dev,set_8_eval,"Set 08", "PCA")
+    score_pca(set_9_train,set_9_dev,set_9_eval,"Set 09", "PCA")
+    score_pca(set_10_train,set_10_dev,set_10_eval,"Set 10", "PCA")
+
+    
     # score the lda for each set
     #
     score_model(set_8_train,set_8_dev,set_8_eval,"Set 08", "LDA")
     score_model(set_9_train,set_9_dev,set_9_eval,"Set 09", "LDA")
     score_model(set_10_train,set_10_dev,set_10_eval,"Set 10", "LDA")
 
-    # score the lda for each set
+    # score the qda for each set
+    #
+    score_model(set_8_train,set_8_dev,set_8_eval,"Set 08", "QDA")
+    score_model(set_9_train,set_9_dev,set_9_eval,"Set 09", "QDA")
+    score_model(set_10_train,set_10_dev,set_10_eval,"Set 10", "QDA")
+    
+    # score the knn for each set
     #
     score_knn(set_8_train,set_8_dev,set_8_eval,"Set 08")
     score_knn(set_9_train,set_9_dev,set_9_eval,"Set 09")
     score_knn(set_10_train,set_10_dev,set_10_eval,"Set 10")
 
-    # score the lda for each set
+    # score the knm for each set
     #
     score_knm(set_8_train,set_8_dev,set_8_eval,"Set 08")
     score_knm(set_9_train,set_9_dev,set_9_eval,"Set 09")
     score_knm(set_10_train,set_10_dev,set_10_eval,"Set 10")
 
-    # score the lda for each set
+    # score the rnf for each set
     #
     score_model(set_8_train,set_8_dev,set_8_eval,"Set 08", "RNF")
     score_model(set_9_train,set_9_dev,set_9_eval,"Set 09", "RNF")
     score_model(set_10_train,set_10_dev,set_10_eval,"Set 10", "RNF")
+
+    
 if __name__ == "__main__":
     main()
